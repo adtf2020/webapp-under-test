eb85937e014024540bbd60861cd858f6b30a5c2b
Walid bra <walidbra@outlook.com>
2023-10-31 10:53:55 +0100

    Code review

--- src/main1.js
+++ src/main1.js
@@ -1,6 +1,3 @@
-// regenerator-runtime is to support async/await syntax in ESNext.
-// If you don't use async/await, you can remove regenerator-runtime.
-import 'regenerator-runtime/runtime';
 import environment from '../config/environment.json';
 import {PLATFORM} from 'aurelia-pal';
 
